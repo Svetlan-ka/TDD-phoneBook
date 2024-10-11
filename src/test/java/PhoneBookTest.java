8e206ac7f733f95674409e18ecd52aaed8269e89
Svetlan-ka <sv.chapova@yandex.ru>
2024-10-12 01:47:50 +0700

    Stub and tests for findByNumber

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.Matchers.equalTo;
 public class PhoneBookTest {
 
     @Test
-    public void addReturnNumberContacts() {
+    public void add_ReturnNumberContacts() {
         //подготавливаем данные
          int expected = 1;
         //вызываем целевой метод
@@ -15,6 +15,28 @@ public class PhoneBookTest {
         //производим проверку
         assertThat(expected, equalTo(count));
     }
+
+    @Test
+    public void findByNumber_ReturnNameContact() {
+        //подготавливаем данные
+        String expectedName = "Sveta";
+        //вызываем целевой метод
+        String name = PhoneBook.findByNumber(645248);
+        //производим проверку
+        assertThat(name, equals(expectedName));
+    }
+
+    @Test
+    public void findByNumber_ReturnNull() {
+        //подготавливаем данные
+        String expectedName = null;
+        //вызываем целевой метод
+        String name = PhoneBook.findByNumber(645148);
+        //производим проверку
+        assertThat(name, equals(expectedName));
+    }
+
+
 
 
 
