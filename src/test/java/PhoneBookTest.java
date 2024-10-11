c9d59fdde7aa4ede424acd2dfc52bfba513b20fa
Svetlan-ka <sv.chapova@yandex.ru>
2024-10-12 01:24:15 +0700

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,21 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class PhoneBookTest {
+
+    @Test
+    public void addReturnNumberContacts() {
+        //подготавливаем данные
+         int expected = 1;
+        //вызываем целевой метод
+        int count = PhoneBook.add();
+        //производим проверку
+        assertThat(expected, equalTo(count));
+    }
+
+
+
 }
